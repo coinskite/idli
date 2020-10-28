1e22f601f56ba5dadf598bff2f6108a528f05dd7
coinskite <coinskite@gmail.com>
2020-10-28 10:32:29 +0530

    idli

---
+++ precache-manifest.71670a7e687ba7b153886a6d3c7f2d7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b7ada64d45fea973165a471253b369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ce9ab81021f4f784e8",
+    "url": "/static/css/main.02a99672.chunk.css"
+  },
+  {
+    "revision": "a45b3d8177c48aadf5f5",
+    "url": "/static/js/2.6c27c86f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c27c86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ce9ab81021f4f784e8",
+    "url": "/static/js/main.3a3682af.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "629f3583207d58001c1d07826887ea6e",
+    "url": "/static/media/Jst.629f3583.png"
+  },
+  {
+    "revision": "04d1cc5275c2d1d6f229b257bafd76a9",
+    "url": "/static/media/Sun.04d1cc52.png"
+  },
+  {
+    "revision": "b4eed5c246f57319127aa31d135db80c",
+    "url": "/static/media/USDT.b4eed5c2.png"
+  },
+  {
+    "revision": "34609c7cf60265481bf83dbefaead98b",
+    "url": "/static/media/idly.34609c7c.png"
+  }
+]);
